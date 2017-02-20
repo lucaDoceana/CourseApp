920ef63168f4f62ccede9f6e1f2ec07f4a0f6068
lucaDoceana <lucad.oceana@gmail.com>
2017-02-20 11:13:06 +0100

    Fase xml

--- app/src/main/java/com/example/luca/courseapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/luca/courseapplication/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.luca.courseapplication;
+package com.example.luca.courseapplication.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.luca.courseapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/luca/courseapplication/models/NoteModel.java
@@ -0,0 +1,8 @@
+package com.example.luca.courseapplication.models;
+
+/**
+ * Created by Luca on 20/02/2017.
+ */
+
+public class NoteModel {
+}
